205ffa280a112bbe7fd3ae1d8cf5e5fd495e8467
Hans Allendorfer <hansallendorfer@gmail.com>
2016-04-26 16:39:28 +0000

    use default server instead of sockets for trashmix (read: bluemix)

--- server.py
+++ server.py
@@ -3,20 +3,20 @@ from flask.ext.socketio import SocketIO, emit
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret!'
-socketio = SocketIO(app)
+# socketio = SocketIO(app)
 
 @app.route('/')
 def index():
     return render_template('index.html')
     
-@socketio.on('connect')
-def connect():
-    print 'connection'
-    emit('my response', {'data': 'Connected'})
+# @socketio.on('connect')
+# def connect():
+#     print 'connection'
+#     emit('my response', {'data': 'Connected'})
 
-@socketio.on('disconnect')
-def disconnect():    
-    print 'disconnect'
+# @socketio.on('disconnect')
+# def disconnect():    
+#     print 'disconnect'
     
 if __name__ == '__main__':
-    socketio.run(app,host='0.0.0.0',port=8080)
+    app.run(host='0.0.0.0',port=8080)
